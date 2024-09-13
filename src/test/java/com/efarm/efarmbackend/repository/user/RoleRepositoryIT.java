f156affeaa822c29a37f3085173b1ceae2f22804
Sprysio <99020634+Sprysio@users.noreply.github.com>
2024-09-13 02:27:07 +0200

    Integration tests

---
+++ src/test/java/com/efarm/efarmbackend/repository/user/RoleRepositoryIT.java
@@ -0,0 +1,65 @@
+package com.efarm.efarmbackend.repository.user;
+
+import com.efarm.efarmbackend.model.user.ERole;
+import com.efarm.efarmbackend.model.user.Role;
+import java.util.Optional;
+import org.junit.jupiter.api.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@DataJpaTest
+@Transactional	
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("integrationtest")
+public class RoleRepositoryIT {
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Test
+	@DisplayName("Tests that operator role is present")
+	public void testFindsOperatorRoleByName() {
+		//given
+		ERole nameTest=ERole.ROLE_FARM_EQUIPMENT_OPERATOR;
+		
+		//when
+		Optional<Role> foundRole = roleRepository.findByName(nameTest);
+
+		//then
+		assertThat(foundRole.isPresent(),is(true));
+		assertThat(foundRole.get(), notNullValue());
+		assertThat(foundRole.get().getName(), is(nameTest));
+	}
+
+	@Test
+	@DisplayName("Tests that manager role is present")
+	public void testFindsManagerRoleByName() {
+		//given
+		ERole nameTest=ERole.ROLE_FARM_MANAGER;
+		
+		//when
+		Optional<Role> foundRole = roleRepository.findByName(nameTest);
+
+		//then
+		assertThat(foundRole.isPresent(),is(true));
+		assertThat(foundRole.get(), notNullValue());
+		assertThat(foundRole.get().getName(), is(nameTest));
+	}
+
+	@Test
+	@DisplayName("Tests that non existing role does not exist")
+	public void testDoesNotFindNonExistingRoleByName() {
+		//when
+		Optional<Role> foundRole = roleRepository.findByName(null);
+
+		//then
+		assertThat(foundRole.isPresent(),is(false));
+	}
+}
