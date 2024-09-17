1178b3255e3d20ef7133a00caf00ad1206996386
BartoszSkowyra22 <skowyrab@gmail.com>
2024-09-17 23:13:49 +0200

    DEV-61 GET all farm users

--- src/main/java/com/efarm/efarmbackend/service/FarmService.java
+++ src/main/java/com/efarm/efarmbackend/service/FarmService.java
@@ -1,9 +1,13 @@
 package com.efarm.efarmbackend.service;
 
 import com.efarm.efarmbackend.model.farm.Farm;
+import com.efarm.efarmbackend.model.user.User;
 import com.efarm.efarmbackend.repository.farm.FarmRepository;
+import com.efarm.efarmbackend.repository.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -12,6 +16,9 @@ public class FarmService {
     @Autowired
     private FarmRepository farmRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public Farm createFarm(String farmName, Integer addressId, Integer activationCodeId) {
         Farm farm = new Farm(farmName);
         farm.setIdAddress(addressId);
@@ -19,6 +26,10 @@ public class FarmService {
         farm.setIsActive(true);
         farmRepository.save(farm);
         return farm;
+    }
+
+    public List<User> getUsersByFarmId(Integer farmId) {
+        return userRepository.findByFarmId(farmId);
     }
 }
 
