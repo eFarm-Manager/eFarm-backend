906e130bb977ee3e2ca142f643fe480887cf886c
BartoszSkowyra22 <skowyrab@gmail.com>
2024-09-17 21:52:35 +0200

    DEV-68 Create CRON to deactivate farms with expired Activation Code

---
+++ src/main/java/com/efarm/efarmbackend/service/ScheduledTasks.java
@@ -0,0 +1,26 @@
+package com.efarm.efarmbackend.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduledTasks {
+
+    @Autowired
+    private FarmService farmService;
+
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    @Scheduled(cron = "0 0 0 * * *", zone = "Europe/Warsaw")
+    public void checkFarmsForExpiredActivationCodes() {
+        logger.info("Start checking expired Activation Codes");
+        try {
+            farmService.deactivateFarmsWithExpiredActivationCodes();
+        } catch (RuntimeException e) {
+            logger.error("Can not deactivate farms with error {}",  e.getMessage());
+        }
+    }
+}
