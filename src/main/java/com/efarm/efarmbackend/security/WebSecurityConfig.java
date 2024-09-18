01a304affc05c5766e5d6c1a048fc5d059fe3c44
BartoszSkowyra22 <skowyrab@gmail.com>
2024-09-18 12:27:05 +0200

    DEV-71 Add actuator health

--- src/main/java/com/efarm/efarmbackend/security/WebSecurityConfig.java
+++ src/main/java/com/efarm/efarmbackend/security/WebSecurityConfig.java
@@ -65,6 +65,7 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/auth/**").permitAll()
                                 .requestMatchers("/api/test/**").permitAll()
+                                .requestMatchers("/actuator/health/**").permitAll()
                                 .anyRequest().authenticated()
                 );
 
