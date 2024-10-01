87b11f15dae3839aa9a0dd144ccca5d6a46c47f5
BartoszSkowyra22 <skowyrab@gmail.com>
2024-10-01 20:14:31 +0200

    DEV-99 GET create enchanced switch in FarmEquipmentDataService

--- src/main/java/com/efarm/efarmbackend/service/FarmEquipmentDataService.java
+++ src/main/java/com/efarm/efarmbackend/service/FarmEquipmentDataService.java
@@ -9,67 +9,63 @@ import java.util.List;
 public class FarmEquipmentDataService {
 
     public List<String> getFieldsForCategory(String categoryName) {
-        switch (categoryName) {
-                //Tractors
-            case "Ciągniki rolnicze":
-            case "Ciągniki sadownicze":
-            case "Harwestery":
-            case "Forwardery":
-            case "Ładowarki teleskopowe":
-            case "Ładowarki kołowe":
-            case "Ładowarki burtowe":
-                return Arrays.asList("power", "insurancePolicyNumber", "insuranceExpirationDate", "inspectionExpireDate");
+        return switch (categoryName) {
+            //Tractors
+            case "Ciągniki rolnicze",
+                 "Ciągniki sadownicze",
+                 "Harwestery",
+                 "Forwardery",
+                 "Ładowarki teleskopowe",
+                 "Ładowarki kołowe",
+                 "Ładowarki burtowe" ->
+                    Arrays.asList("power", "insurancePolicyNumber", "insuranceExpirationDate", "inspectionExpireDate");
 
-                //Trailers
-            case "Przyczepy rolnicze":
-            case "Przyczepy do transportu bel":
-            case "Przyczepy do transportu drewna":
-            case "Przyczepy do transportu zwierząt":
-            case "Przyczepy samozbierające":
-            case "Przyczepy pojemnościowe":
-            case "Naczepy":
-            case "Wozy asenizacyjne":
-            case "Wozy paszowe":
-                return Arrays.asList("capacity", "insurancePolicyNumber", "insuranceExpirationDate", "inspectionExpireDate");
+            //Trailers
+            case "Przyczepy rolnicze",
+                 "Przyczepy do transportu bel",
+                 "Przyczepy do transportu drewna",
+                 "Przyczepy do transportu zwierząt",
+                 "Przyczepy samozbierające",
+                 "Przyczepy pojemnościowe",
+                 "Naczepy",
+                 "Wozy asenizacyjne",
+                 "Wozy paszowe" ->
+                    Arrays.asList("capacity", "insurancePolicyNumber", "insuranceExpirationDate", "inspectionExpireDate");
 
-                //Harvesters
-            case "Kombajny zbożowe":
-            case "Kombajny do ziemniaków":
-            case "Kombajny do buraków cukrowych":
-            case "Kombajny do owoców":
-            case "Silosokombajny":
-            case "Kombajny inne":
-                return Arrays.asList("power", "capacity", "workingWidth", "insurancePolicyNumber", "insuranceExpirationDate", "inspectionExpireDate");
+            //Harvesters
+            case "Kombajny zbożowe",
+                 "Kombajny do ziemniaków",
+                 "Kombajny do buraków cukrowych",
+                 "Kombajny do owoców",
+                 "Silosokombajny",
+                 "Kombajny inne" ->
+                    Arrays.asList("power", "capacity", "workingWidth", "insurancePolicyNumber", "insuranceExpirationDate", "inspectionExpireDate");
 
-                //Agricultural machinery
-            case "Agregaty ścierniskowe (Grubery)":
-            case "Brony mechaniczne":
-            case "Brony talerzowe":
-            case "Rębaki":
-            case "Kosiarki":
-            case "Glebogryzarki":
-            case "Głębosze":
-            case "Mulczery":
-            case "Maszyny do uprawy winorośli":
-            case "Wały uprawowe":
-            case "Zgrabiarki":
-            case "Ładowacze czołowe":
-                return Arrays.asList("workingWidth");
+            //Agricultural machinery
+            case "Agregaty ścierniskowe (Grubery)",
+                 "Brony mechaniczne",
+                 "Brony talerzowe",
+                 "Rębaki",
+                 "Kosiarki",
+                 "Glebogryzarki",
+                 "Głębosze",
+                 "Mulczery",
+                 "Maszyny do uprawy winorośli",
+                 "Wały uprawowe",
+                 "Zgrabiarki",
+                 "Ładowacze czołowe" -> Arrays.asList("workingWidth");
 
-                //Sprayers, spreaders etc.
-            case "Opryskiwacze polowe":
-            case "Opryskiwacze sadownicze":
-            case "Opryskiwacze samojezdne":
-            case "Drony do opryskiwania":
-            case "Rozsiewacze nawozów":
-            case "Rozsiewacze wapna":
-            case "Siewniki punktowe":
-            case "Siewniki zbożowe":
-            case "Zbieracze kamieni":
-                return Arrays.asList("capacity", "workingWidth");
-
-            default:
-                return List.of();
-        }
+            //Sprayers, spreaders etc.
+            case "Opryskiwacze polowe",
+                 "Opryskiwacze sadownicze",
+                 "Opryskiwacze samojezdne",
+                 "Drony do opryskiwania",
+                 "Rozsiewacze nawozów",
+                 "Rozsiewacze wapna",
+                 "Siewniki punktowe",
+                 "Siewniki zbożowe",
+                 "Zbieracze kamieni" -> Arrays.asList("capacity", "workingWidth");
+            default -> List.of();
+        };
     }
 }
