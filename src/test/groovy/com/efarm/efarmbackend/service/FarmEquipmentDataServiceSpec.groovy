2e2c7b9475d6ccf2d307e4cf4963d386dd200bc0
Sprysio <99020634+Sprysio@users.noreply.github.com>
2024-10-02 18:37:50 +0200

    unit tests

---
+++ src/test/groovy/com/efarm/efarmbackend/service/FarmEquipmentDataServiceSpec.groovy
@@ -0,0 +1,81 @@
+package com.efarm.efarmbackend.service;
+
+import spock.lang.Specification
+import spock.lang.Subject
+import java.util.Arrays;
+import java.util.List;
+
+
+class EquipmentDisplayDataServiceSpec extends Specification {
+
+    @Subject
+    EquipmentDisplayDataService equipmentDisplayDataService = new EquipmentDisplayDataService()
+
+    def "should return correct fields for tractors"() {
+        given: 
+        String categoryName = "Ciągniki rolnicze"
+
+        when: 
+        List<String> result = equipmentDisplayDataService.getFieldsForCategory(categoryName)
+
+        then: 
+        result == ["power", "insurancePolicyNumber", "insuranceExpirationDate", "inspectionExpireDate"]
+    }
+
+    def "should return correct fields for trailers"() {
+        given: 
+        String categoryName = "Przyczepy rolnicze"
+
+        when: 
+        List<String> result = equipmentDisplayDataService.getFieldsForCategory(categoryName)
+
+        then: 
+        result == ["capacity", "insurancePolicyNumber", "insuranceExpirationDate", "inspectionExpireDate"]
+    }
+
+    def "should return correct fields for harvesters"() {
+        given: 
+        String categoryName = "Kombajny zbożowe"
+
+        when: 
+        List<String> result = equipmentDisplayDataService.getFieldsForCategory(categoryName)
+
+        then: 
+        result == ["power", "capacity", "workingWidth", "insurancePolicyNumber", "insuranceExpirationDate", "inspectionExpireDate"]
+    }
+
+    def "should return correct fields for agricultural machinery"() {
+        given: 
+        String categoryName = "Kosiarki"
+
+        when: 
+        List<String> result = equipmentDisplayDataService.getFieldsForCategory(categoryName)
+
+        then: 
+        result == ["workingWidth"]
+    }
+
+    def "should return correct fields for sprayers and spreaders"() {
+        given: 
+        String categoryName = "Opryskiwacze polowe"
+
+        when: 
+        List<String> result = equipmentDisplayDataService.getFieldsForCategory(categoryName)
+
+        then: 
+        result == ["capacity", "workingWidth"]
+    }
+
+    def "should return an empty list for unrecognized category"() {
+        given: 
+        String categoryName = "Unknown category"
+
+        when: 
+        List<String> result = equipmentDisplayDataService.getFieldsForCategory(categoryName)
+
+        then: 
+        result == []
+    }
+
+
+}
