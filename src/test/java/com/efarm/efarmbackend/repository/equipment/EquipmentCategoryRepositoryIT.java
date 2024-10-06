ef2cb3757a200448afe994a65050abc98f8cf125
Sprysio <99020634+Sprysio@users.noreply.github.com>
2024-10-06 18:46:07 +0200

    integration tests

---
+++ src/test/java/com/efarm/efarmbackend/repository/equipment/EquipmentCategoryRepositoryIT.java
@@ -0,0 +1,68 @@
+package com.efarm.efarmbackend.repository.equipment;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.efarm.efarmbackend.model.equipment.EquipmentCategory;
+
+
+@DataJpaTest
+@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("integrationtest")
+public class EquipmentCategoryRepositoryIT {
+
+    @Autowired
+    EquipmentCategoryRepository equipmentCategoryRepository;
+
+    @Test
+    @DisplayName("Tests finding all category names")
+    public void testFindAllCategoryNames() {
+        //when
+        List<String> allCategory = equipmentCategoryRepository.findAllCategoryNames();
+
+        //then
+        assertThat(allCategory.size(), is(61));
+    }
+
+    @Test
+    @DisplayName("Tests finding category by its name")
+    public void testFindByCategoryName() {
+        //given
+        String categoryName = "Ciągniki rolnicze";
+
+        //when
+        EquipmentCategory foundCategory = equipmentCategoryRepository.findByCategoryName(categoryName);
+        
+        //then
+        assertThat(foundCategory, is(notNullValue()));
+        assertThat(foundCategory.getCategoryName(), is("Ciągniki rolnicze"));
+    }
+
+    @Test
+    @DisplayName("Tests not finding category by its name")
+    public void testotFindByCategoryName() {
+        //given
+        String categoryName = "doesnt exist";
+
+        //when
+        EquipmentCategory foundCategory = equipmentCategoryRepository.findByCategoryName(categoryName);
+        
+        //then
+        assertThat(foundCategory, is(nullValue()));
+    }
+
+    
+}
--- src/test/java/com/efarm/efarmbackend/repository/equipment/FarmEquipmentRepositoryIT.java
+++ src/test/java/com/efarm/efarmbackend/repository/equipment/FarmEquipmentRepositoryIT.java
@@ -8,6 +8,7 @@ import static org.hamcrest.Matchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,6 +20,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.efarm.efarmbackend.model.equipment.FarmEquipment;
+import com.efarm.efarmbackend.model.equipment.FarmEquipmentId;
+import com.efarm.efarmbackend.model.farm.ActivationCode;
+import com.efarm.efarmbackend.model.farm.Address;
+import com.efarm.efarmbackend.model.farm.Farm;
 
 @DataJpaTest
 @Transactional
@@ -45,10 +50,119 @@ public class FarmEquipmentRepositoryIT {
         List<FarmEquipment> farmEquipmentsList = farmEquipmentRepository.findByFarmIdFarm_Id(farmId);
 
         //then
-        System.out.println(farmEquipmentsList);
         assertThat(farmEquipmentsList, not(empty()));
         assertThat(countActive.intValue(), is(farmEquipmentsList.size()));
         assertThat(farmEquipmentsList, everyItem(hasProperty("id", hasProperty("farmId", is(farmId)))));
-
     }
+
+    @Test
+    @DisplayName("Tests that equipment exists in farm by its name")
+    public void testEquipmentExistsByNameAndFarmId() {
+        //given
+        FarmEquipmentId farmEquipmentId = new FarmEquipmentId(1,1);
+        FarmEquipment farmEquipment = entityManager.find(FarmEquipment.class, farmEquipmentId);
+        String equipmentName = farmEquipment.getEquipmentName();
+        Farm farm = entityManager.find(Farm.class, farmEquipment.getId().getFarmId());
+
+        //when
+        Boolean exists = farmEquipmentRepository.existsByEquipmentNameAndFarmIdFarm(equipmentName,farm);
+
+        //then
+        assertThat(exists, is(true));
+    }
+
+    @Test
+    @DisplayName("Tests that equipment does not exists in farm by its name")
+    public void testEquipmentDoesNotExistByNameAndFarmId() {
+        //given
+        Farm farm = entityManager.find(Farm.class, 1);
+
+        //when
+        Boolean exists = farmEquipmentRepository.existsByEquipmentNameAndFarmIdFarm("Nonexistent Equipment", farm);
+
+        //then
+        assertThat(exists, is(false));
+    }
+
+    @Test
+    @DisplayName("Tests that it returns max id for equipment in farm")
+    public void testFindMaxEquipmentIdForFarm() {
+        //given
+        Farm farm = entityManager.find(Farm.class, 1);
+
+        Integer maxId = entityManager.getEntityManager()
+        .createQuery("SELECT MAX(e.id.id) FROM FarmEquipment e WHERE e.id.farmId = 1", Integer.class)
+        .getSingleResult();
+
+        //when
+        Optional<Integer> maxIdFound = farmEquipmentRepository.findMaxIdForFarm(farm.getId());
+
+        //then
+        System.out.println(maxId);
+        assertThat(maxIdFound.get(),is(maxId));
+    }
+
+    @Test
+    @DisplayName("Tests that it returns id 1 for equipment in farm that doesnt have any equipment yet")
+    public void testFindMaxEquipmentIdForNewFarm() {
+        //given
+        Address address = new Address();
+        entityManager.persist(address);
+        entityManager.flush();
+        ActivationCode activationCode = entityManager.find(ActivationCode.class, 1);
+        Farm farm = new Farm();
+        farm.setFarmName("uniqueFarmName");
+        farm.setIsActive(true);
+        farm.setIdAddress(address.getId());
+        farm.setIdActivationCode(activationCode.getId());
+        entityManager.persist(farm);
+        entityManager.flush();
+
+        //when
+        Optional<Integer> maxIdFound = farmEquipmentRepository.findMaxIdForFarm(farm.getId());
+
+        //then
+        assertThat(maxIdFound,is(Optional.empty()));
+    }
+
+    @Test
+    @DisplayName("Tests that it returns next free id for equipment in farm")
+    public void testFindNextMaxFreeEquipmentIdForFarm() {
+        //given
+        Farm farm = entityManager.find(Farm.class, 1);
+
+        Integer maxId = entityManager.getEntityManager()
+        .createQuery("SELECT MAX(e.id.id) FROM FarmEquipment e WHERE e.id.farmId = 1", Integer.class)
+        .getSingleResult();
+
+        //when
+        Integer maxIdFound = farmEquipmentRepository.findNextFreeIdForFarm(farm.getId());
+
+        //then
+        assertThat(maxIdFound,is(maxId+1));
+    }
+
+    @Test
+    @DisplayName("Tests that it returns next free id for equipment in new farm")
+    public void testFindNextMaxFreeEquipmentIdForNewFarm() {
+        //given
+        Address address = new Address();
+        entityManager.persist(address);
+        entityManager.flush();
+        ActivationCode activationCode = entityManager.find(ActivationCode.class, 1);
+        Farm farm = new Farm();
+        farm.setFarmName("uniqueFarmName");
+        farm.setIsActive(true);
+        farm.setIdAddress(address.getId());
+        farm.setIdActivationCode(activationCode.getId());
+        entityManager.persist(farm);
+        entityManager.flush();
+
+        //when
+        Integer maxIdFound = farmEquipmentRepository.findNextFreeIdForFarm(farm.getId());
+
+        //then
+        assertThat(maxIdFound,is(1));
+    }
+
 }
