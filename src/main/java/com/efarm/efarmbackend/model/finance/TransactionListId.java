f4463b7f14a20667135d24f9229ff35cc6f07056
BartoszSkowyra22 <skowyrab@gmail.com>
2024-10-08 21:11:33 +0200

    DEV-127 Add finance models

--- src/main/java/com/efarm/efarmbackend/model/finance/TransactionListId.java
+++ src/main/java/com/efarm/efarmbackend/model/finance/TransactionListId.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.Hibernate;
 
 import java.io.Serial;
 import java.util.Objects;
