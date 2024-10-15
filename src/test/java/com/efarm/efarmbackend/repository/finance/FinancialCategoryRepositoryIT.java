a0c394903ee30bce72bd141236113e2699647b75
Sprysio <99020634+Sprysio@users.noreply.github.com>
2024-10-15 15:10:00 +0200

    integration test

---
+++ src/test/java/com/efarm/efarmbackend/repository/finance/FinancialCategoryRepositoryIT.java
@@ -0,0 +1,50 @@
+package com.efarm.efarmbackend.repository.finance;
+import org.junit.jupiter.api.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.not;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import com.efarm.efarmbackend.model.finance.EFinancialCategory;
+import com.efarm.efarmbackend.model.finance.FinancialCategory;
+
+@DataJpaTest
+@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("integrationtest")
+public class FinancialCategoryRepositoryIT {
+    @Autowired
+    private FinancialCategoryRepository financialCategoryRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+	
+	@Test
+	public void testFindIncomeByName() {
+		// when
+		FinancialCategory found = financialCategoryRepository.findByName(EFinancialCategory.INCOME);
+		
+		// then
+		assertThat(found.getName(), is(EFinancialCategory.INCOME));
+	}
+	
+	@Test
+	public void testFindExpenseByName() {
+		// when
+		FinancialCategory found = financialCategoryRepository.findByName(EFinancialCategory.EXPENSE);
+		
+		// then
+		assertThat(found.getName(), is(EFinancialCategory.EXPENSE));
+	}
+}
