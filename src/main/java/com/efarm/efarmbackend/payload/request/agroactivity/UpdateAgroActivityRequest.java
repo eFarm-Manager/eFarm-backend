74efa95c6c5b66ca6a58ec629e5f1e28beb5f339
BartoszSkowyra22 <skowyrab@gmail.com>
2024-11-08 19:06:05 +0100

    DEV-198 Changes after Review

--- src/main/java/com/efarm/efarmbackend/payload/request/agroactivity/UpdateAgroActivityRequest.java
+++ src/main/java/com/efarm/efarmbackend/payload/request/agroactivity/UpdateAgroActivityRequest.java
@@ -16,11 +16,11 @@ public class UpdateAgroActivityRequest {
 
     @NotNull(message = "Kategoria zabiegu nie może być pusta")
     @NotBlank(message = "Kategoria zabiegu nie może być pusta")
-    @Size(max = 45, message = "Nazwa kategroii zabiegu nie może przekraczać 45 znaków.")
+    @Size(max = 45, message = "Nazwa kategorii zabiegu nie może przekraczać 45 znaków.")
     private String activityCategoryName;
 
     @NotNull(message = "Kategoria zabiegu nie może być pusta")
-    @NotBlank(message = "Nazwa zabiegu zabiegu nie może być pusta")
+    @NotBlank(message = "Nazwa zabiegu nie może być pusta")
     @Size(max = 45, message = "Nazwa zabiegu nie może przekraczać 45 znaków.")
     private String name;
 
