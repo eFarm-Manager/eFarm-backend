e7120304e2d89e82c66ca4051a142cdc2b0c1fd1
BartoszSkowyra22 <skowyrab@gmail.com>
2024-11-10 19:01:05 +0100

    DEV-213 Add Repository annotations

--- src/main/java/com/efarm/efarmbackend/repository/finance/PaymentStatusRepository.java
+++ src/main/java/com/efarm/efarmbackend/repository/finance/PaymentStatusRepository.java
@@ -3,7 +3,10 @@ package com.efarm.efarmbackend.repository.finance;
 import com.efarm.efarmbackend.model.finance.EPaymentStatus;
 import com.efarm.efarmbackend.model.finance.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PaymentStatusRepository extends JpaRepository<PaymentStatus, Integer> {
+
     PaymentStatus findByName(EPaymentStatus paymentStatusEnum);
 }
