19527ba65a5fe3be777c235336e1c0777913a749
BartoszSkowyra22 <skowyrab@gmail.com>
2024-11-11 16:46:52 +0100

    DEV-212 Fix CI errors

--- src/main/java/com/efarm/efarmbackend/payload/request/equipment/AddUpdateFarmEquipmentRequest.java
+++ src/main/java/com/efarm/efarmbackend/payload/request/equipment/AddUpdateFarmEquipmentRequest.java
@@ -1,13 +1,17 @@
 package com.efarm.efarmbackend.payload.request.equipment;
 
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddUpdateFarmEquipmentRequest {
     private Integer equipmentId;
 
